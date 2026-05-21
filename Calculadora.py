12e14a950a00d8fe28c6409ba8eaa010944ded07
Ives Diniz Cezar <ivesao@gmail.com>
2026-05-21 20:14:31 -0300

    atualização calculadora

--- Calculadora.py
+++ Calculadora.py
@@ -2,7 +2,7 @@
 import flet as ft
 def main(page: ft.Page):
     
-    page.title = "Calculadora "
+    page.title = "Calculadora"
     numero1 = ft.TextField(label="Primeiro Valor")
     numero2 = ft.TextField(label="Segundo Valor")
     resultado = ft.Text("Resultado: ")
@@ -10,10 +10,21 @@ def main(page: ft.Page):
     def calcular ():
      n1 = float(numero1.value)
      n2 = float(numero2.value)
-     r = n1 + n2
-     resultado.value = f"Resultado: {r}"
-    botao_calcular = ft.ElevatedButton("Calcular", on_click=calcular)
-    
-    page.add(numero1, numero2, resultado, botao_calcular)
+     
+     operacao = operacoes.value
+     
+     
+    operacoes = ft.Dropdown(
+        label = "Escolha a operação",
+        options =[
+            ft.dropdown.Option("+"),
+            ft.dropdown.Option("-"),
+            ft.dropdown.Option("x"),
+            ft.dropdown.Option("/"),             
+            ]
+         )
+
+    page.update()
+    page.add(numero1, numero2, resultado, operacoes)
 
 ft.app(target=main)
