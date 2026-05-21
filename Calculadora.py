a09d72ed07cde8e258da694574c2a779c15b9821
Ives Diniz Cezar <ivesao@gmail.com>
2026-05-21 20:50:35 -0300

    Atualização

--- Calculadora.py
+++ Calculadora.py
@@ -13,18 +13,35 @@ def main(page: ft.Page):
      
      operacao = operacoes.value
      
-     
+     if operacao == "+":
+         res = n1 + n2
+     elif operacao == "-":
+         res = n1 - n2
+     elif operacao == "x":
+         res = n1 * n2
+     elif operacao == "÷":
+         res = n1 / n2
+     else:
+         resultado.value = "Resultado: Selecione uma operação"
+         page.update()
+         return
+
+     resultado.value = f"Resultado: {res}"
+     page.update()
+
+
     operacoes = ft.Dropdown(
         label = "Escolha a operação",
         options =[
             ft.dropdown.Option("+"),
             ft.dropdown.Option("-"),
             ft.dropdown.Option("x"),
-            ft.dropdown.Option("/"),             
+            ft.dropdown.Option("÷"),             
             ]
          )
 
+    button_calcular = ft.ElevatedButton("Calcular", on_click=calcular)
     page.update()
-    page.add(numero1, numero2, resultado, operacoes)
+    page.add(numero1, numero2,operacoes, button_calcular,resultado )
 
 ft.app(target=main)
